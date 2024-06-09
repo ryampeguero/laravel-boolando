83ca483c5c6a91883866bb2f89ba648c6ef4a2d9
ryampeguero <ryampeguero@gmail.com>
2024-06-09 22:54:03 +0200

    feat: Fontawesome installed and imported in app.scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,29 +1,35 @@
-
 <header>
     <div class="container">
         <div class="row navbar d-flex justify-content-between">
             <div class="col-3 border menu">
-                <ul style="padding: 0"> 
+                <ul style="padding: 0">
                     <li>
                         <a href="">Donna</a>
                     </li>
-                    
+
                     <li>
                         <a href="">Uomo</a>
                     </li>
-                    
+
                     <li>
                         <a href="">Bambino</a>
                     </li>
                 </ul>
             </div>
             <div class="col-3 border ms_cell">
-                <img src={{Vite::asset('resources/img/boolean-logo.png')}} alt="">
+                <img src={{ Vite::asset('resources/img/boolean-logo.png') }} alt="">
             </div>
             <div class="col-3 border ms_cell">
-                loghi per dopo
+                <div class="menu">
+                    <div class="icons">
+                        <ul style="padding: 0">
+                            <li><a href=""><i class="fa-solid fa-user"></i></a></li>
+                            <li><a href=""><i class="fa-regular fa-heart"></i></a></li>
+                            <li><a href=""><i class="fa-solid fa-bag-shopping"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </header>
-
